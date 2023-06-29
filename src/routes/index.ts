9e9fb508e4a567bfb71db3d16f535a630a0d7396
Jason <jasonwong287962@gmail.com>
2023-06-28 19:55:25 -0500

    link to github on base link

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req: Request, res: Response, next: NextFunction) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Smooth Dining API" });
 });
 
 module.exports = router;
