9b78a2a2fe5c7f0e6041fc386b01a08d3669cc46
Jason <jasonwong287962@gmail.com>
2023-06-30 13:43:11 -0500

    update validators and status and timeData route

--- src/controller/party_Controller.ts
+++ src/controller/party_Controller.ts
@@ -2,13 +2,13 @@ import { Request, Response, NextFunction } from "express";
 import Party from "../model/stores/Party";
 import { dateRegex, helperFunctions } from "./helper_Controller";
 import { body, cookie } from "express-validator";
-declare module "express" {
-  interface Request {
-    headers: {
-      storeID?: string;
-    };
-  }
-}
+// declare module "express" {
+//   interface Request {
+//     headers: {
+//       storeID?: string;
+//     };
+//   }
+// }
 
 const partyController = {
   queryAllPartyToday: async (
@@ -62,8 +62,48 @@ const partyController = {
     res: Response,
     next: NextFunction
   ) => {},
-  setPartyDining: async (req: Request, res: Response, next: NextFunction) => {},
-  setPartyStatus: async (req: Request, res: Response, next: NextFunction) => {},
+
+  setPartyTimeData: async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.partyID;
+    // const timeData = {
+    //   checkInTime: req.body.checkInTime,
+    //   startDining: {
+    //     time: req.body.startDining,
+    //     isEntreeOnTable: req.body.isEntreeOnTable,
+    //   },
+    //   finishedTime: req.body.finishedTime,
+    //   waitingTime: req.body.waitingTime,
+    // };
+    const timeData = req.body;
+    try {
+      const timeDataFilter = Object.fromEntries(
+        Object.entries(timeData).filter(([key, value]) => value != undefined)
+      );
+
+      const result = Party.updateOne({ _id: id }, { $set: timeDataFilter });
+      res.json({ result: result, message: "Party Time Updated Successfully" });
+    } catch (e) {
+      res.status(400).json({ message: "Failed to Update Party Time Data" });
+    }
+  },
+  setPartyStatus: async (req: Request, res: Response, next: NextFunction) => {
+    const store = req.cookies.storeID;
+    const id = req.params.partyID;
+    const status = req.body.status;
+
+    try {
+      const result = Party.updateOne({ _id: id }, { $set: status });
+
+      res.json({ message: "New Status:" + status, result: result });
+    } catch (e) {
+      res.status(400).json({
+        e: e,
+        message: "Failed to Update Status",
+        id: id,
+        storeID: store,
+      });
+    }
+  },
 
   validation: {
     validateCreatePartyData: [
@@ -74,9 +114,26 @@ const partyController = {
         .matches(dateRegex)
         .withMessage("format: new Date() objects | YYYY-MM-DDTHH:mm:ss.SSSZ"),
       body("checkInTime").optional().matches(dateRegex),
-      body("status").optional(),
+      body("status")
+        .optional()
+        .isIn(["Active", "Finished", "Canceled"])
+        .withMessage("Active, Finished, or Canceled Only"),
       body("phoneNumber").isMobilePhone(["en-US"], { strictMode: false }),
       cookie("storeID").escape().notEmpty(),
+      helperFunctions.expressValidationMiddleware,
+    ],
+    validatePartyTimeData: [
+      body("checkInTime").matches(dateRegex).optional(),
+      body("time")
+        .matches(dateRegex)
+        .optional()
+        .withMessage("for startDining Time: Must be of type Data"),
+      body("isEntreeOnTable").isBoolean().optional(),
+      helperFunctions.expressValidationMiddleware,
+      // body("waitingTime").matches(dateRegex).optional(),
+    ],
+    validateStatusData: [
+      body("status").escape().isIn(["Active", "Finished", "Canceled"]),
       helperFunctions.expressValidationMiddleware,
     ],
   },
--- src/routes/ApiRouter/store/party.ts
+++ src/routes/ApiRouter/store/party.ts
@@ -10,4 +10,14 @@ router.post(
   partyController.validation.validateCreatePartyData,
   partyController.createNewParty
 );
+router.put(
+  "/timeData/:partyID",
+  partyController.validation.validatePartyTimeData,
+  partyController.setPartyTimeData
+);
+router.put(
+  "/status/:partyID",
+  partyController.validation.validateStatusData,
+  partyController.setPartyStatus
+);
 module.exports = router;
