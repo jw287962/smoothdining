ebaf010e72e592fbe6641899ef158eab4637922b
Jason <jasonwong287962@gmail.com>
2023-07-05 19:31:38 -0500

    removed set

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -10,7 +10,7 @@ export const userController = {
   userLogin: (req: Request, res: Response, next: NextFunction) => {
     const { username, password } = req.body;
     const user = req.user as UserInterface;
-    res.setHeader("Set-Cookie", `user=${JSON.stringify(user._id)}; Path=/`);
+    // res.setHeader("Set-Cookie", `user=${JSON.stringify(user._id)}; Path=/`);
 
     res.json({
       message: "login successfully. Welcome" + username,
