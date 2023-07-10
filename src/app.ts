2502e9c47bf6433932b0298ed3ea5c24941d65c1
Jason <jasonwong287962@gmail.com>
2023-07-10 03:21:02 -0500

    add res.headers

--- src/app.ts
+++ src/app.ts
@@ -104,10 +104,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res: Response, next: NextFunction) => {
-  // res.header(
-  // "Access-Control-Allow-Headers",
-  // "Content-Type, POST, GET, OPTIONS, DELETE"
-  // );
+  // res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Credentials", "true"); // allow sending and receiving cookies
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
