abeb5b112b1f65343566409114b13c6947b71367
Jason <jasonwong287962@gmail.com>
2023-07-10 03:54:20 -0500

    set cookie path

--- src/controller/store_controller.ts
+++ src/controller/store_controller.ts
@@ -53,7 +53,7 @@ export const storecontroller = {
         const data = await Store.findById(new ObjectId(storeID));
         res.setHeader(
           "Set-Cookie",
-          `storeid=${storeID}; Path=/api/account/store; HttpOnly; Secure; SameSite=none`
+          `storeid=${storeID}; Path=/; HttpOnly; Secure; SameSite=none`
         );
         // ;HttpOnly; Secure;
         res.json({ store: data, results: storeID });
