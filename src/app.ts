c06cb6140f390411947701eb187aa90c5c72e834
Jason <jasonwong287962@gmail.com>
2023-07-17 20:22:47 -0500

    rate limit @ 60/min

--- src/app.ts
+++ src/app.ts
@@ -92,7 +92,7 @@ app.set("view engine", "pug");
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 30,
+  max: 60,
 });
 
 // Apply rate limiter to all requests
